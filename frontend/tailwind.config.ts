f5f4a475875052e5144be097b4393e52ab194740
Jagadeeshftw <jagadeesh26062002@gmail.com>
2024-07-15 02:50:43 +0530

    Adding new styles

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -255,6 +255,7 @@ const config: Config = {
       "2xl": "0 25px 50px -12px rgb(0 0 0 / 0.25)",
       inner: "inset 0 2px 4px 0 rgb(0 0 0 / 0.05)",
       none: "none",
+      input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
     },
     boxShadowColor: ({ theme }) => theme("colors"),
     caretColor: ({ theme }) => theme("colors"),
