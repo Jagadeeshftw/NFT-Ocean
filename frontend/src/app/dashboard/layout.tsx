12ce7a1d040bd1ab46a9fc69b0c817a47e22c9f0
Jagadeeshftw <jagadeesh26062002@gmail.com>
2024-07-21 15:57:18 +0530

    Adding layout for the dashboard

--- frontend/src/app/dashboard/layout.tsx
+++ frontend/src/app/dashboard/layout.tsx
@@ -1,8 +1,10 @@
 "use client";
 import Sidebar from "@/components/Sidebar/Sidebar";
 import { SidebarContext } from "./context/SidebarContext";
-import { useContext, useEffect } from "react";
+import { Suspense, useContext, useEffect } from "react";
 import { usePathname } from "next/navigation";
+import DashboardNavbar from "@/components/Dashboard/DashboardNavbar";
+import ThemedSuspense from "@/components/Dashboard/ThemedSuspense";
 
 export default function RootLayout({
   children,
@@ -11,10 +13,10 @@ export default function RootLayout({
 }>) {
   const { isSidebarOpen, closeSidebar } = useContext(SidebarContext);
   const pathname = usePathname();
-  console.log("is side", isSidebarOpen);
+  console.log("isSidebarOpen in layout", isSidebarOpen);
   useEffect(() => {
     closeSidebar();
-  }, [pathname, closeSidebar]);
+  }, [pathname]);
 
   return (
     <html lang="en">
@@ -25,8 +27,14 @@ export default function RootLayout({
           }`}
         >
           <Sidebar />
-          <div className="flex flex-col flex-1 w-full"></div>
-          {children}
+          <div className="flex flex-col flex-1 w-full">
+            <DashboardNavbar />
+            <main className="h-full overflow-y-auto">
+              <div className="container grid px-6 mx-auto h-full w-full">
+                <Suspense fallback={<ThemedSuspense />}>{children}</Suspense>
+              </div>
+            </main>
+          </div>
         </div>
       </body>
     </html>
