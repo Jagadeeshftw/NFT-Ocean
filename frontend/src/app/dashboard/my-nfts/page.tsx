b6788d64bd4f22c18db18ee643194d19da5db086
Jagadeesh B <jagadeesh.b@velocix.com>
2024-07-26 00:26:08 +0530

    Adding my nfts page

--- frontend/src/app/dashboard/my-nfts/page.tsx
+++ frontend/src/app/dashboard/my-nfts/page.tsx
@@ -1,7 +1,80 @@
-import React from "react";
+"use client"
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import { initProvider } from "@/lib";
 
-const page = () => {
-  return <div>my-nfts</div>;
+
+type Item = {
+  price: string;
+  tokenId: string;
+  seller: string;
+  owner: string;
+  sold: boolean;
+  image: string;
+  name: string;
+  desc: string;
 };
 
-export default page;
+const Page = () => {
+     const [items, setItems] = useState<Item[]>([]);
+
+    useEffect(() => {
+      initConfig();
+    },[]);
+    const initConfig = async () => {
+      try {
+        const { OwnedNFTs} = await initProvider();
+
+        setItems(OwnedNFTs);
+        
+      } catch (error) {
+        console.error("Error initializing configuration:", error);
+
+      }
+
+      console.log(items);
+    };
+
+   
+
+  return (
+    <div>
+            {!items.length ?
+            <div className="products-heading">
+            <h2>You have not owned any NFTs</h2>
+          </div>:
+      <>
+      <div className="products-heading">
+      <h2>Discover All Unique Digital Art</h2>
+      <p>Explore and own exclusive NFTs created by talented artists from around the world.</p>
+    </div>
+
+    <div className="products-container">
+      {items?.map((product) => (
+        <div key={product.tokenId}>
+          <Link href={`/dashboard/all-nfts/${product.name}`}>
+            <div className="product-card">
+              <Image
+                src={product.image}
+                width={250}
+                height={250}
+                className="product-image"
+                alt="image"
+              />
+              <p className="product-name">{product.name}</p>
+              <p className="product-price">{product.price} MATIC</p>
+            </div>
+          </Link>
+        </div>
+
+      ))}
+    </div>
+    </>
+      }
+      
+    </div>
+  );
+};
+
+export default Page;
